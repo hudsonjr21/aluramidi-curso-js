42425f5fecf74f97dcff29a0127e1a50f28bc15b
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-05-15 14:21:27 -0300

    Parte 4 - Finalizado

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-const listaDeTeclas = document.querySelectorAll('input[type=button]');
-console.log(listaDeTeclas);
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    // console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    // console.log(contador);
+}
+
